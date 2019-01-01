2df37fd6863fa338dd555ce25f8af33d879b3498
Khoa Bui <thomasbui93@gmail.com>
2019-01-01 13:12:17 +0000

    add basic setup for typescript project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header className="header">
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
